9b28a72c1738dee72d2a591f3c3bf9b528d8c04a
Filipe A. Oliveira <filipewk@gmail.com>
2020-06-18 20:37:12 -0300

    test: ensure SignUp route returns an account on success

---
+++ src/main/routes/signup-router.test.ts
@@ -0,0 +1,19 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('SignUp Routes', () => {
+  test('Should return an account on success', async () => {
+    app.post('/api/signup', (req, res) => {
+      res.send()
+    })
+    await request(app)
+      .post('/api/signup')
+      .send({
+        name: 'Filipe',
+        email: 'filipewk@gmail.com',
+        password: '123456',
+        passwordConfirmation: '123456'
+      })
+      .expect(200)
+  })
+})
