0628eb4d8ce580f30a10d254b3975fcc81f4507c
Filipe A. Oliveira <filipewk@gmail.com>
2020-06-24 20:23:23 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://root:filipe50@172.19.0.3:27017/clean-node-api?authSource=admin',
-  port: process.env.PORT || 5050
+  port: process.env.PORT || 5050,
+  jwtSecret: process.env.JWT_SECRET || 'Tkz(*&^%$sax$#$%^&^%==1'
 }
