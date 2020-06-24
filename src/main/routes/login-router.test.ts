42ec54928ab4383c781960d7d65eae644b08cda3
Filipe A. Oliveira <filipewk@gmail.com>
2020-06-24 20:33:34 -0300

    test: login route return 401 with invalid credentials are provided

--- src/main/routes/login-router.test.ts
+++ src/main/routes/login-router.test.ts
@@ -50,5 +50,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    test('Should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'filipewk@gmail.com',
+          password: '123456'
+        })
+        .expect(401)
+    })
   })
 })
