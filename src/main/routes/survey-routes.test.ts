bcad6dc225c5fa575a45c34570005108b552d6a1
Filipe <filipewk@gmail.com>
2020-07-01 21:18:13 -0300

    test: ensure add survey route returns 403 if no accessToken is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -18,7 +18,7 @@ describe('Survey Routes', () => {
   })
 
   describe('POST /surveys', () => {
-    test('Should return 204 on add survey success', async () => {
+    test('Should return 403 on add survey without accessToken', async () => {
       app.post('/api/surveys', (req, res) => {
         res.send()
       })
@@ -35,7 +35,7 @@ describe('Survey Routes', () => {
             }
           ]
         })
-        .expect(204)
+        .expect(403)
     })
   })
 })
