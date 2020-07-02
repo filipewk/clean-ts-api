31df27faa201b637cccd14e143263fee137bbbe1
Filipe <filipewk@gmail.com>
2020-07-01 21:17:59 -0300

    feat: ensure add survey route returns 403 if no accessToken is provided

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -1,5 +1,6 @@
 import { Controller, HttpRequest } from '../../presentation/protocols'
 import { Request, Response } from 'express'
+
 export const adaptRoute = (controller: Controller) => {
   return async (req: Request, res: Response) => {
     const httpRequest: HttpRequest = {
