e1e5bb733d539bbc3d9b70d696d2c390e8c02512
filipewk <filipewk@gmail.com>
2020-07-02 21:49:34 -0300

    test: ensure SurveyMongoRepository load all surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -40,4 +40,28 @@ describe('SurveyMongo Repository', () => {
       expect(survey).toBeTruthy()
     })
   })
+
+  describe('loadAll()', () => {
+    test('Should loadAll surveys on success', async () => {
+      await surveyCollection.insertMany([{
+        question: 'any_question',
+        answers: [{
+          image: 'any_image',
+          answer: 'any_answer'
+        }],
+        date: new Date()
+      }, {
+        question: 'other_question',
+        answers: [{
+          image: 'other_image',
+          answer: 'other_answer'
+        }],
+        date: new Date()
+      }])
+      const sut = makeSut()
+      const surveys = await sut.loadAll()
+      expect(surveys[0].question).toBe('any_question')
+      expect(surveys[1].question).toBe('other_question')
+    })
+  })
 })
