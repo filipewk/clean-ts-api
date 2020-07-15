7986b581f8f813b6e2ec2e461a5d2f8e3cfd1891
filipewk <filipewk@gmail.com>
2020-07-14 22:40:24 -0300

    feat: add route to serve static files and update dependencies

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,10 +1,12 @@
 import setupMiddlewares from './middlewares'
 import setupeRoutes from './routes'
+import setupeStaticFiles from './static-files'
 import setupSwagger from './config-swagger'
 import express from 'express'
 
 const app = express()
 app.disable('x-powered-by')
+setupeStaticFiles(app)
 setupSwagger(app)
 setupMiddlewares(app)
 setupeRoutes(app)
